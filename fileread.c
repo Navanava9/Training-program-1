ffa50e18c3615f9d6d1d523a66da0fd73d2cd88b
Navanava9 <40741930@qq.com>
2021-02-23 12:39:12 +0800

    1

--- fileread.c
+++ fileread.c
@@ -6,7 +6,6 @@
 PNode fileread(void)
 {
     char y;
-    char *ch;
     PNode hand = NULL;
     FILE *user_login = fopen("C:\\Users\\Administrator.DESKTOP-53KMUVB\\Desktop\\code\\Training-program-1\\user_login.txt", "r");
     if (user_login == NULL)
@@ -15,11 +14,11 @@ PNode fileread(void)
     {
         DataType a;
         int x = 0;
-        ch = (char *)malloc(sizeof(char) * LENGTH);
-        fgets(ch, LENGTH, user_login);
+        char *s = (char *)malloc(LENGTH * sizeof(char));
+        fgets(s, LENGTH, user_login);
         while (1)
         {
-            y = ch[x];
+            y = s[x];
             if (y == ',')
                 break;
             a.name[x] = y;
@@ -27,7 +26,7 @@ PNode fileread(void)
         }
         a.totalcount = 1;
         PushBack(&hand, a);
-        free(ch);
+        free(s);
     }
     fclose(user_login);
     return hand;
